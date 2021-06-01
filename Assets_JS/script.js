64c5107c799841431c54b956c3a01ca13d674b79
RyanCasf <ryaninternet62@gmail.com>
2021-05-31 21:57:46 -0300

    Assests of the JS and molde bank imagens site.

---
+++ Assets_JS/script.js
@@ -0,0 +1,12 @@
+// Banco de Consumo
+// alert('https://source.unsplash.com/!')
+
+const urlBase = "https://source.unsplash.com/random/";
+
+function getImagens(String key1)
+{
+	const urlImg('${urlBase}300x300')
+}
+
+getImagens();
+
